bfd677c55586424b9662a2fe5bdba106bcd4c4f7
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-10 14:25:42 +0000

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower_alphabet;
+
+	for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
+	{
+		if (lower_alphabet == 'e' || lower_alphabet == 'q')
+		{
+			continue;
+		}
+		putchar(lower_alphabet);
+		putchar('\n');
+	}
+	return (0);
+}
