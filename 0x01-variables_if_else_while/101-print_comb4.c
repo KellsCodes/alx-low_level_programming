b242695cffe08ed5eaceaf6898b0fd30b65fca3f
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-11 00:14:03 +0000

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; ++j)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar(i % 10 + '0');
+					putchar(j % 10 + '0');
+					putchar(k % 10 + '0');
+
+					if (i == 7 && j == 8 && k == 9)
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
