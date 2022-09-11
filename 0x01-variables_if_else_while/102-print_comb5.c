7ad46b0a764c376b9dfed470a7198eb410419039
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-11 01:05:01 +0000

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,9 +18,10 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			
-			if (i == 99 - 1 && j == 100 - 1)
+			if (i == 98 && j == 99)
+			{
 				continue;
+			}
 			putchar(',');
 			putchar(' ');
 		}
