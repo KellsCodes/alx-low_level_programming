3c0cf9d7e0b8e2c1f8c73762f8a97fe636eeae32
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-13 17:56:15 +0000

    c-function that prints the english alphabets in lowercase without using the standard header file included

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+	return (0);
+}
