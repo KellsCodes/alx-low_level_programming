840eb75b7ea69c3575a87ec31e60d01dbf5f0cee
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-14 05:00:37 +0000

    completed task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - return the absolute value of a number
+ * @number: input value as an integer
+ * Description: prints the absolute value of a number
+ * Return: return an integer absolute value
+ */
+int _abs(int number)
+{
+	return (abs(number));
+}
