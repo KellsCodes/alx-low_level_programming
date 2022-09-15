4b091a97eb9c3b67f16afc1ac479176c43ff0d23
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-15 21:58:27 +0000

    c function that prints 10 times the numbers from 0 to 14 using putchar only

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 10; ++i)
+	{
+		int j;
+
+		for (j = 0; j < 15; ++j)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
