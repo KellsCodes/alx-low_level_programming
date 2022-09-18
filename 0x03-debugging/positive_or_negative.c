d84b8adfe029f8b078346fca488add7bac1b0215
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-18 20:32:09 +0000

    update

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks the sign of a number
+ * @i: input number
+ * Return:void
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d", 0);
+	else if (i == 0)
+		printf("%d is zero", 0);
+	printf("%d", 1);
+	printf("\n");
+}
