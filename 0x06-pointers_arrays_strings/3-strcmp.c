3cbf9d6d7b17b15b49c0148cd0fec30a43ea20f7
KellsCodes <nworjiifeanyi@gmail.com>
2022-09-22 21:49:11 +0000

    string compare and return conditional int value

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,20 +12,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len = 0, s2_len = 0;
-
-	while (*s1)
+	while (*s1 && *s2)
 	{
-		s1_len++, s1++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s2++;
+		s1++;
 	}
 
-	while (*s2)
-		s2_len++, s2++;
-
-	if (s1_len == s2_len)
-		return (s1_len - s2_len);
-	else if (s1_len > s2_len)
-		return (s1_len - s2_len);
-	else
-		return (s1_len - s2_len);
+	return (*s1 - *s2);
 }
