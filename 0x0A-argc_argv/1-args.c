ace75279dfe7dca09d163a6d87af8e753a36d061
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-08 05:21:51 +0000

    update on argc and argv in c projects

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
