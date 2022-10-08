c6e39349577ada0e286ab186f5f982db36fc2427
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-08 15:05:04 +0000

    pointer to a new memory location after contenating two string pointers

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s3 = malloc(strlen(s1) + n);
+	char *s3 = malloc(strlen(s1) + n + 1);
 	int i = 0, j = 0;
 
 	if (s3 == NULL)
