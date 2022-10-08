fedca86ca27a587f3f97754542ef927e5c4b79f8
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-08 16:46:12 +0000

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,11 +8,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *memallocated;
+	char *memallocated;
+	unsigned int i = 0;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
 
 	memallocated = malloc(nmemb * size);
-
-	if (nmemb <= 0 || size <= 0 || memallocated == NULL)
+	if (memallocated == NULL)
 		return (NULL);
+	for (; i < nmemb * size; i++)
+		*(memallocated + i) = 0;
+
 	return (memallocated);
 }
