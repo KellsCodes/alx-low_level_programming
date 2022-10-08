d47d368f9596ab98035891cd731751ffb70ea2b1
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-08 16:26:37 +0000

    memory allocation using calloc and setting each block to 0

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _calloc - allocate memory for an array set to 0
+ *
+ * @nmemb: the number of memory blocks to be allocated
+ * @size: the size in bytes for each of the block of memory allocated
+ * Return: pointer to allocated memory or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *memallocated;
+
+	memallocated = calloc(nmemb, size);
+
+	if (nmemb == 0 || size == 0 || memallocated == NULL)
+		return (NULL);
+	return (memallocated);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,7 @@
 #include <limits.h>
 #include <string.h>
 
+void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif /*MAIN_H*/
