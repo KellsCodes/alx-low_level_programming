d811a04e6dcef4050b7787bc02d00bcb847ddcd8
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-08 12:37:53 +0000

    pointer to dynamic memory allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory to the pointer argument b
+ *
+ * @b: pointer argument for memory allocation
+ *
+ * Return: pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	return (malloc(b));
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,7 @@
+0x0C. C - More malloc, free
+
+Reference materials:
+https://alx-intranet.hbtn.io/concepts/62
+
+Resources:
+https://alx-intranet.hbtn.io/rltoken/uKhvfzpF3v8Be10NCZlQtA
