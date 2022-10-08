30fbadde2cfbcdf8961a912a87d75c63beb0061c
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-08 14:34:10 +0000

    pointer to a new memory location after contenating two string pointers

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concats two string values from pointers
+ *
+ * @s1: pointer to first string value
+ * @s2: pointer to second string value
+ * @n: dynamic memory size allocated for the ponter s3
+ * Return: pointer to new memory allocation contains s1
+ * followed by n bytes of s2 and null terminated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3 = malloc(strlen(s1) + n);
+	int i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	if (s3 == NULL)
+		exit(98);
+	else
+	{
+		while (*s1 != '\0')
+		{
+			s3[i] = *s1;
+			s1++, i++;
+		}
+
+		while (n > 0)
+		{
+			s3[i] = s2[j];
+			n -= 1;
+			i += 1;
+			j += 1;
+		}
+	}
+
+	s3[i] = '\0';
+
+	return (s3);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
 
 void *malloc_checked(unsigned int b);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif /*MAIN_H*/
