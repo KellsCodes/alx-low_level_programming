b5358681937ad6bf4fb00898cf277b059b60d143
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-24 11:09:25 +0000

    defined a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dob_obj;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
