d4cf46215297eddb12d641773de9fa1f536a6f13
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-24 11:02:54 +0000

    function that conditionally prints struct dog if a member is null or available

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,21 +7,12 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name != NULL)
-			printf("Name: %s\n", (*d).name);
-		else
-			printf("(nil)\n");
-		
-		if (d->age >= 0)
-			printf("Age: %f\n", d->age);
-		else
-			printf("(nil)\n");
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
 
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("(nil)\n");
-	}
+	printf("Name: %s\nAge: %.1f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
