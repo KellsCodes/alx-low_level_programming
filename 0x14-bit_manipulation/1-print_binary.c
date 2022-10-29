b6c480b9ae9d675dafcb84d2fe1e6308b2c1f64d
KellsCodes <nworjiifeanyi@gmail.com>
2022-10-29 01:16:51 +0000

    update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: number to print in binary
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+
+}
